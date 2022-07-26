8bb335eecec1e68afdfee24b22a5de5fc13616a0
Aman kumar Yadav <107494596+AmanYadavB@users.noreply.github.com>
2022-07-26 11:18:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO-NAME>.git master:gh-pages
+git push -f git@github.com:<AmanYadavB>/<Meeting-App>.git master:gh-pages
 
 cd -
